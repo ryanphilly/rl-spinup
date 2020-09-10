3e60afead9d7b3ccd09dbe310314abb0a24c01f3
Ryan Phillips <ryphillips15@outlook.com>
2020-09-10 04:18:51 -0500

    ok

--- RL-Barto-Sutton/cart_pole.py
+++ RL-Barto-Sutton/cart_pole.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.keras import layers, Model
 
 class DeepQAproximator(Model):
-  def __init__(self, observation_shape, num_actions, discount_factor=1e0, epsilon=1e-1, *args, **kwargs):
+  def __init__(self, observation_shape, num_actions, discount_factor=1e0, epsilon=1e-1, **kwargs):
     super(QAproximator, self).__init__()
     self.dense_relu1 = layers.Dense(32, input_shape=observation_shape, activation=tf.nn.relu)
     self.dense_relu2 = layers.Dense(32, activation=tf.nn.relu)
@@ -11,4 +11,11 @@ class DeepQAproximator(Model):
     self.compile(**kwargs)
 
   def call(self, observation, training=False):
+    Y = self.dense_relu1(observation)
+    Y = self.dense_relu2(Y)
+    return self.dense_action(Y)
+
+class CartPoleControl(object):
+  def __init__(self, discount_factor=1e0, epsilon=1e-1):
+    self.env = gym.make('cartpole-v1')
     pass
